316f4b51af677022c2d6ae47f505e6fb45fea5c8
ScubaSteve721 <george72142@gmail.com>
2024-05-22 11:55:07 -0400

    BILL

---
+++ BILLBILL.java
@@ -0,0 +1,39 @@
+package AAA;
+
+import java.util.ArrayList;
+
+public class BILLBILL {
+	
+	private ArrayList<String> orderedFood;
+	private ArrayList<Integer> orderedQuantity;
+	public BILLBILL() {
+		
+		orderedFood = new ArrayList<String>();
+		orderedQuantity = new ArrayList<Integer>();
+	}
+	
+	private double totalCost;
+	public double getTotal() {
+		return totalCost;
+	}
+	
+	public void addOrder(String meal, int quantity, String[] dish, double[] cost) {
+		orderedFood.add(meal);
+		orderedQuantity.add(quantity);
+		for(int i = 0; i < dish.length; i++) {
+			if(meal.equals(dish[i])) {
+				totalCost += quantity * cost[i];
+			}
+		}
+	}
+	
+		public void getOrder() {
+			for(int i = 0; i < orderedFood.size(); i++) {
+				System.out.println(orderedFood.get(i) +
+						" " + orderedQuantity.get(i));
+			}
+		}
+		
+	}
+
+
---
+++ BILLBILLDRIVER.java
@@ -0,0 +1,21 @@
+package AAA;
+
+public class BILLBILLDRIVER {
+	
+	public static void main(String[] args) {
+		
+		String[] dish = {"Sandwich", "Coffee", "French Fries", "Salad"};
+		double[] cost = {6.5, 3.2, 4.0, 3.5};
+		BILLBILL myBill = new BILLBILL();
+		
+		myBill.addOrder("Sandwich", 2, dish, cost);
+		myBill.addOrder("Coffee", 2, dish, cost);
+		myBill.addOrder("French Fries", 1, dish, cost);
+		myBill.addOrder("Salad", 1, dish, cost);
+		myBill.getOrder();
+		
+		System.out.println(myBill.getTotal());
+		
+	}
+
+}
